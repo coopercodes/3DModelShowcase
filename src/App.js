2a4e3d3f9dcefcc7f5b82caf0360ae6e17862100
coopercodes <thecoopercodes@gmail.com>
2022-11-29 11:38:31 -0800

    Project push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]} />
+      <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
   );
 }
 
